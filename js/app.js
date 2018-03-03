27d7f3cbde26b613c02432daf923d77a265936ba
Cory Harkins <CoryHarkins@Corys-MacBook-Pro.local>
2018-03-03 00:30:08 -0600

    added station toggle

--- js/app.js
+++ js/app.js
@@ -7,26 +7,62 @@ let $albumArt = $('#album_art');
 let $station = $('#station');
 let $desc = $('#desc');
 let $stationURL = $('#station_url');
-
+let $pause = $('#pause');
+let $play = $('#play');
+let $reverse = $('#reverse');
+let $forward = $('#forward');
+let stationIndex = 9;
+let stationLength = 0;
 
 $.get(PROXY_URL+API_URL, function(data){
-	console.log(data);
 	
 	String.prototype.capitalizeDesc = function(){
 		return this.charAt(0).toUpperCase() + this.slice(1);
 	}
-	let description = data[1].categories[0].description;
-	$albumArt.css('background-image',`url(${data[1].image.url})`)
-	$station.html(data[1].name);
-	$desc.html(description.capitalizeDesc());
-	$stationURL.html(`<a href="${data[1].website}">Visit us @${data[1].name}</a>`);
 
+	String.prototype.hasBreak = function(){
+		return this.replace(/\r?\n|\r/gm,"");
+	}
+	let stream = data[stationIndex].streams[0].stream;
+	let playStation = new Audio(stream.hasBreak());
+	playStation.play();
 
+	function updateStation(i){
 
-    let playStation = new Audio(`${data[1].streams[0].stream}`);
-    playStation.play();
+			let description = data[stationIndex].categories[0].description;
+			let stationLength = data.length;
+			if (stationIndex > stationLength) {
+				stationIndex = 0;
+			}
+
+			$albumArt.css('background-image',`url(${data[stationIndex].image.url})`)
+			$station.html(data[stationIndex].name);
+			$desc.html(description.capitalizeDesc());
+			$stationURL.html(`<a href="${data[stationIndex].website}">Visit us @${data[stationIndex].name}</a>`);
+	}
+
+	updateStation(stationIndex);
+
+	$forward.click(function(){
+  		playStation.src = '';
+  		stationIndex++;
+  		updateStation(stationIndex)
+  		console.log(stationIndex);
+  		let streamUpdate = data[stationIndex].streams[0].stream;
+  		streamUpdate = streamUpdate.hasBreak();
+  		playStation.src = streamUpdate;
+  		playStation.play();	
+  });
+  $reverse.click(function(){
+  		playStation.src = '';
+  		stationIndex--;
+  		updateStation(stationIndex)
+  		console.log(stationIndex);
+  		let streamUpdate = data[stationIndex].streams[0].stream;
+  		streamUpdate = streamUpdate.hasBreak();
+  		playStation.src = streamUpdate;
+  		playStation.play();	
+  });
 
 });
-
-
 });
